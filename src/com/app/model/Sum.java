2516da8c2e675fa09944e821866f0d356548ae62
ravi <ravi@DESKTOP-R808QMS>
2018-12-01 20:35:28 +0530

    wh 4569

---
+++ src/com/app/model/Sum.java
@@ -0,0 +1,9 @@
+package com.app.model;
+
+public class Sum {
+public int add(int x,int y) {
+	int z=x+y;
+	return z;
+	//System.out.println(z);
+}
+}
---
+++ src/com/app/test/Test.java
@@ -0,0 +1,11 @@
+package com.app.test;
+
+import com.app.model.Sum;
+
+public class Test {
+public static void main(String[] args) {
+	Sum s=new Sum();
+	int z=s.add(10,20);
+	System.out.println(z);
+}
+}
